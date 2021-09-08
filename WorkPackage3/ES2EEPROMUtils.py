db1dfc790cc348ef42b0050802f644275a507e90
Mark Njoroge <marksnjoroge@gmail.com>
2021-09-08 10:52:15 +0200

    modified eepromutils.py

--- WorkPackage3/ES2EEPROMUtils.py
+++ WorkPackage3/ES2EEPROMUtils.py
@@ -2,7 +2,8 @@
 Code originally by CRImier
 https://www.raspberrypi.org/forums/viewtopic.php?p=1401819#p1401819
 
-Modified by Keegan Crankshaw for EEE3096S 2020
+Modified to work with Microchip's 24LC16B
+
 """
 
 from smbus2 import SMBus as SMBus2, i2c_msg
@@ -15,13 +16,13 @@ class ES2EEPROM:
         self.bus = bus
         self.address = address
 
-    def write_block(self, start_block, data, bs=32, sleep_time=0.01):
+    def write_block(self, start_block, data, bs=16, sleep_time=0.01):
         """
         Write data in blocks, starting at pos start_block.
 
         :param start_block: The starting block
         :param data: The data to write
-        :param bs: The block size. Set at 32 for this EEPROM
+        :param bs: The block size. Set at 16 for this EEPROM
         :param sleep_time: A default value to delay between operations
 
         """
@@ -37,8 +38,8 @@ class ES2EEPROM:
                 sleep(sleep_time)
             start = i*bs+start_block
             hb, lb = start >> 8, start & 0xff
-            data = [hb, lb]+block
-            write = i2c_msg.write(self.address, data)
+            data = [lb]+block
+            write = i2c_msg.write((self.address + hb), data)
             self.bus.i2c_rdwr(write)
 
     def write_byte(self, reg, data):
@@ -51,18 +52,18 @@ class ES2EEPROM:
         """
 
         hb, lb = reg >> 8, reg & 0xff
-        data = [hb, lb, data]
-        write = i2c_msg.write(self.address, data)
+        data = [lb, data]
+        write = i2c_msg.write((self.address + hb), data)
         self.bus.i2c_rdwr(write)
         sleep(0.01)
 
-    def read_block(self, start_block, count, bs=32):
+    def read_block(self, start_block, count, bs=16):
         """
         Reads multiple registers starting at a given block.
 
         :param start_block: The starting block
-        :param count: THe amount of registers to read
-        :param bs: Standard block size of 32 bits
+        :param count: The amount of registers to read
+        :param bs: Standard block size of 16 bits
         :return: None
 
         """
@@ -77,10 +78,10 @@ class ES2EEPROM:
         for i in range(full_reads):
             start = i*bs+start_block  # next block address
             hb, lb = start >> 8, start & 0xff  # into high and low byte
-            write = i2c_msg.write(self.address, [hb, lb])
+            write = i2c_msg.write((self.address + hb), [lb])
             # If we're on last cycle and remainder != 0, not doing a full read
             count = remainder if (remainder and i == full_reads-1) else bs
-            read = i2c_msg.read(self.address, count)
+            read = i2c_msg.read((self.address + hb), count)
             self.bus.i2c_rdwr(write, read)  # combined read&write
             data += list(read)
         return data
@@ -94,8 +95,8 @@ class ES2EEPROM:
 
         """
         hb, lb = reg >> 8, reg & 0xff  # into high and low byte
-        write = i2c_msg.write(self.address, [hb, lb])
-        read = i2c_msg.read(self.address, 1)
+        write = i2c_msg.write((self.address + hb), [lb])
+        read = i2c_msg.read((self.address + hb), 1)
         self.bus.i2c_rdwr(write, read)  # combined read&write
         return list(read)[0]
 
@@ -119,17 +120,16 @@ class ES2EEPROM:
         self.write_block(0, [4])
         scores = [["ChB", 5], ["Ada", 7], ["LSu", 4], ["EEE", 8]]
         scores.sort(key=lambda x: x[1])
-        data_to_write = []
-        for score in scores:
+        for i, score in enumerate(scores):
+            data_to_write = []
             # get the string
             for letter in score[0]:
                 data_to_write.append(ord(letter))
             data_to_write.append(score[1])
-        self.write_block(1, data_to_write)
+            self.write_block(i+1, data_to_write)
 
 
 if __name__ == "__main__":
     eeprom = ES2EEPROM()
-    eeprom.clear(4096)
+    eeprom.clear(2048)
     eeprom.populate_mock_scores()
-
